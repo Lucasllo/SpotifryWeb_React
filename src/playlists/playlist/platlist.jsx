5b1fed1efcc3da80d538388f9d2bdf87c2e23a8e
Lucas Lopes <lucaslopesoliveira@gmail.com>
2022-04-07 11:37:14 -0300

    costumizacao das playlists

--- src/playlists/playlist/platlist.jsx
+++ src/playlists/playlist/platlist.jsx
@@ -1,20 +1,31 @@
 import { listas } from './estilosMusicais/estilosMusicais';
 import '../playlists.css'
 import { useParams } from 'react-router';
+import playlists from '../playlists_imagens/playlists_imagens';
 
 function Playlist() {
     const params = useParams();
     return (
-        <div>
-            <ul className="">
-                {listas().at(params.id-1).lista.map((item) => {
-                    return (
-                        <audio controls>
-                            <source src={item.musica} type="audio/mpeg" />
-                        </audio>
-                    )
-                })}
-            </ul>
+        <div className="playlist">
+
+            <div className="playlist_capa">
+                <figure className="playlist_capa_campo">
+                    <img src={`/${playlists().at(params.id-1).imagem}`} alt="capa"/>
+                </figure>
+            </div>
+
+            <div className="playlist_musicas">
+                <ul>
+                    {listas().at(params.id - 1).lista.map((item) => {
+                        return (
+                            <audio controls>
+                                <source src={item.musica} type="audio/mpeg" />
+                            </audio>
+                        )
+                    })}
+                </ul>
+
+            </div>
         </div>
     )
 }
